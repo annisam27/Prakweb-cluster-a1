01048cd074ca0a65e22250027ae1eb55d62738dd
Annisa Maharani <maharaniannisa80@gmail.com>
2020-11-15 19:27:45 +0700

    commit tugas, latihan modul 4 dan 5

---
+++ minggu2/Modul 5/Tugas/lat2.php
@@ -0,0 +1,8 @@
+<?php
+
+$nama = array("lanirne", "aduh", "qifuat", "toda", "anevi", "samid", "kifuat", "asinna", "sayit", "lurun");
+sort($nama);
+for ($i = 0; $i <= 9; $i++) {
+    $z = $nama[$i];
+    echo $z . "<br />";
+}
